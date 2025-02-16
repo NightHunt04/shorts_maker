f64bee87aaced4837172d2542c6d9b4fd634114e
NightHunt04 <jeetbherwani2004@gmail.com>
2025-02-16 13:45:15 +0530

    fixed issue: bgm err in 'add without b-roll'

--- utils/add_without_broll.py
+++ utils/add_without_broll.py
@@ -41,36 +41,37 @@ def add_without_broll(
                 'music/astroscape-motivation.mp3',
                 'music/adventure-music-prime-facts.mp3',
             ]
-            if int(background_music) >= 0 and int(background_music) < len(bgm):
-                bgm = bgm[int(background_music)]
-                add_subtitles(
-                    segments = segments, 
-                    input_video = f"media/cropped_clips/{video_uuid}/{ind}.mp4", 
-                    output_path = f"media/final_clips/{video_uuid}/{ind}_without_bgm.mp4", 
-                    video_uuid = video_uuid, 
-                    words_per_subtitle = words_per_subtitle, 
-                    kinetic_subtitles = kinetic_subtitles,
-                    font_size = font_size,
-                    font_color = font_color,
-                    background_color = background_color,
-                    font_highlight_color = font_highlight_color,
-                    italic = italic,
-                    bold = bold,
-                    font_highlight_size = font_highlight_size,
-                    subtitles_position = subtitles_position,
-                    font_family = font_family,
-                )
-                video = VideoFileClip(f"media/final_clips/{video_uuid}/{ind}_without_bgm.mp4")
-                music = AudioFileClip(bgm).set_duration(video.duration).volumex(background_music_volume)
-                combined_audio = CompositeAudioClip([video.audio, music])
-                video = video.set_audio(combined_audio)
-                video.write_videofile(
-                    f"media/final_clips/{video_uuid}/{ind}.mp4", 
-                    codec = "libx264",
-                    audio_codec = "aac",
-                    threads = THREADS_TO_BE_USED
-                )
-                print(f'✅ - made clip {ind} without b-roll successfully')
+            if background_music:
+                if int(background_music) >= 0 and int(background_music) < len(bgm):
+                    bgm = bgm[int(background_music)]
+                    add_subtitles(
+                        segments = segments, 
+                        input_video = f"media/cropped_clips/{video_uuid}/{ind}.mp4", 
+                        output_path = f"media/final_clips/{video_uuid}/{ind}_without_bgm.mp4", 
+                        video_uuid = video_uuid, 
+                        words_per_subtitle = words_per_subtitle, 
+                        kinetic_subtitles = kinetic_subtitles,
+                        font_size = font_size,
+                        font_color = font_color,
+                        background_color = background_color,
+                        font_highlight_color = font_highlight_color,
+                        italic = italic,
+                        bold = bold,
+                        font_highlight_size = font_highlight_size,
+                        subtitles_position = subtitles_position,
+                        font_family = font_family,
+                    )
+                    video = VideoFileClip(f"media/final_clips/{video_uuid}/{ind}_without_bgm.mp4")
+                    music = AudioFileClip(bgm).set_duration(video.duration).volumex(background_music_volume)
+                    combined_audio = CompositeAudioClip([video.audio, music])
+                    video = video.set_audio(combined_audio)
+                    video.write_videofile(
+                        f"media/final_clips/{video_uuid}/{ind}.mp4", 
+                        codec = "libx264",
+                        audio_codec = "aac",
+                        threads = THREADS_TO_BE_USED
+                    )
+                    print(f'✅ - made clip {ind} without b-roll successfully')
             else:
                 add_subtitles(
                     segments = segments, 
